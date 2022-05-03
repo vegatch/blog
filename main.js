c37e5b938dc6cde9a207a266215cd11757c915f4
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2022-05-03 17:12:21 -0400

    made email work

--- main.js
+++ main.js
@@ -15,43 +15,66 @@ document.getElementById('menu-display').appendChild(template.content);
 
 
 
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init("XhKv7nyqdxTaERRBm");
-})();
+// (function() {
+//     // https://dashboard.emailjs.com/admin/account
+//     emailjs.init("XhKv7nyqdxTaERRBm");
+// })();
    
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
+// window.onload = function() {
+//     document.getElementById('contact-form').addEventListener('submit', function(event) {
+//         event.preventDefault();
+//         // generate a five digit number for the contact_number variable
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         // these IDs from the previous steps
         
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                    console.log('FAILED...', error);
-                });
+//         emailjs.sendForm('contact_service', 'contact_form', this)
+//             .then(function() {
+//                 console.log('SUCCESS!');
+//             }, function(error) {
+//                     console.log('FAILED...', error);
+//                 });
+//             });
+//         }
+
+
+//     var templateParams = {
+//     name: 'Dude',
+//     notes: 'Check this out!'
+// };
+ 
+// emailjs.send('yvecKa1979', 'template_1xqzy6o', templateParams)
+//     .then(function(response) {
+//        console.log('SUCCESS!', response.status, response.text);
+//     }, function(error) {
+//        console.log('FAILED...', error);
+//     });
+
+// emailjs.sendForm('yvecKa1979', 'template_1xqzy6o', '#myForm')
+//     .then(function(response) {
+//        console.log('SUCCESS!', response.status, response.text);
+//     }, function(error) {
+//        console.log('FAILED...', error);
+//     });
+
+
+
+
+ (function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init('XhKv7nyqdxTaERRBm');
+        })();
+    
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate a five digit number for the contact_number variable
+                this.contact_number.value = Math.random() * 100000 | 0;
+                // these IDs from the previous steps
+                emailjs.sendForm('service_3yjgpu2', 'template_1xqzy6o', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
             });
         }
-
-
-    var templateParams = {
-    name: 'Dude',
-    notes: 'Check this out!'
-};
- 
-emailjs.send('yvecKa1979', 'template_1xqzy6o', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-
-emailjs.sendForm('yvecKa1979', 'template_1xqzy6o', '#myForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
