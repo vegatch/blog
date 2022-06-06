f6476b9f955c013f8e11566ba2eabc5111e1f712
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2022-06-06 09:28:44 -0400

    Adding my resume in the browser

--- resume.js
+++ resume.js
@@ -160,7 +160,7 @@ skillsP.textContent = 'SKILLS'
 
 let skillsDetailDiv = document.createElement("div")
 let skillsTechDiv = document.createElement("div")
-skillsTechDiv.classList.add("flex-container")
+skillsTechDiv.classList.add("flex-container", "skills-section-container")
 let skillsTechP = document.createElement("p")
 skillsTechP.classList.add("text-bold")
 skillsTechP.textContent ="Technical Skills:"
@@ -180,7 +180,7 @@ skillsTechSix.textContent ="MySQL."
 skillsTechDiv.append(skillsTechP, skillsTechOne, skillsTechTwo, skillsTechThree, skillsTechFour, skillsTechFive, skillsTechSix)
 
 let skillsMarketDiv = document.createElement("div")
-skillsMarketDiv.classList.add("flex-container")
+skillsMarketDiv.classList.add("flex-container", "skills-section-container")
 let skillsMarketP = document.createElement("p")
 skillsMarketP.classList.add("text-bold")
 skillsMarketP.textContent ="Marketable Skills:"
@@ -281,7 +281,7 @@ realizationThreeDiv.append(spanRealizationThree, realizationThreeP)
 let projectsDetailSecondDiv = document.createElement("div")
 projectsDetailSecondDiv.classList.add("flex-container")
 
-let newLine = document.createElement("br")
+let projectNewLine = document.createElement("br")
 
 let projectsLeftSecondDiv = document.createElement("div")
 projectsLeftSecondDiv.classList.add("flex-container", "left-row") // to review class name ====================
@@ -340,7 +340,7 @@ realizationThreeSecondDiv.append(spanRealizationThreeSecond, realizationThreeSec
 projectsDetailDiv.append(projectsLeftDiv, projectRightDiv)
 projectsDetailSecondDiv.append(projectsLeftSecondDiv, projectRightSecondDiv)
 
-projectsDiv.append(projectSectionDiv, projectsDetailDiv, projectsLocationDiv, realizationOneDiv, realizationTwoDiv, realizationThreeDiv, newLine, projectsDetailSecondDiv, projectsLocationSecondDiv, realizationOneSecondDiv, realizationTwoSecondDiv, realizationThreeSecondDiv)
+projectsDiv.append(projectSectionDiv, projectsDetailDiv, projectsLocationDiv, realizationOneDiv, realizationTwoDiv, realizationThreeDiv, projectNewLine, projectsDetailSecondDiv, projectsLocationSecondDiv, realizationOneSecondDiv, realizationTwoSecondDiv, realizationThreeSecondDiv)
 
 
 // Projects section : End
@@ -355,11 +355,14 @@ workHeaderP.classList.add("headerSection", "text-bold", 'top-margin')
 workHeaderP.textContent = "WORK EXPERIENCE"
 workHeaderDiv.append(workHeaderP)
 
+let workNewLine = document.createElement("br")
+
 let firstRowOneDiv = document.createElement("div")
 firstRowOneDiv.classList.add("flex-container")
 let firstRowOneLeftDiv = document.createElement("div")
 firstRowOneLeftDiv.classList.add("left-row")
 let firstRowOneLeftP =document.createElement("p")
+firstRowOneLeftP.classList.add("text-bold")
 firstRowOneLeftP.textContent = 'Amazon FC'
 firstRowOneLeftDiv.append(firstRowOneLeftP)
 
@@ -381,7 +384,7 @@ firstRowTwoDiv.append(firstRowTwoLeftP)
 let firstRowTwoRightDiv = document.createElement("div")
 firstRowTwoRightDiv.classList.add("right-row")
 let firstRowTwoRightP =document.createElement("p")
-firstRowTwoRightP.classList.add("text-bold")
+// firstRowTwoRightP.classList.add("text-bold")
 firstRowTwoRightP.textContent = 'Sep 2020 - to date'
 firstRowTwoRightDiv.append(firstRowTwoRightP)
 
@@ -423,7 +426,8 @@ secondRowOneDiv.classList.add("flex-container")
 let secondRowOneLeftDiv = document.createElement("div")
 secondRowOneLeftDiv.classList.add("left-row")
 let secondRowOneLeftP =document.createElement("p")
-secondRowOneLeftP.textContent = 'Hubler Ford'
+secondRowOneLeftP.classList.add("text-bold")
+secondRowOneLeftP.textContent = 'Hubler Ford Franklin'
 secondRowOneLeftDiv.append(secondRowOneLeftP)
 
 let secondRowOneRightDiv = document.createElement("div")
@@ -441,12 +445,12 @@ let secondRowTwoLeftDiv = document.createElement("div")
 secondRowTwoLeftDiv.classList.add("left-row")
 let secondRowTwoLeftP =document.createElement("p")
 secondRowTwoLeftP.textContent = 'Salesman'
-secondRowTwoDiv.append(secondRowTwoLeftP)
+secondRowTwoLeftDiv.append(secondRowTwoLeftP)
 
 let secondRowTwoRightDiv = document.createElement("div")
 secondRowTwoRightDiv.classList.add("right-row")
 let secondRowTwoRightP =document.createElement("p")
-secondRowTwoRightP.classList.add("text-bold")
+// secondRowTwoRightP.classList.add("text-bold")
 secondRowTwoRightP.textContent = 'July 2021 - Dec 2021'
 secondRowTwoRightDiv.append(secondRowTwoRightP)
 
@@ -457,7 +461,7 @@ workRealizationOneSecondDiv.classList.add("flex-container")
 let spanWorkRealizationOneSecond = document.createElement("span") 
 spanWorkRealizationOneSecond.classList.add("circle")
 let workRealizationOneSecondP = document.createElement("p")
-workRealizationOneSecondP.textContent = 'Performing six (6) sided checks on item to avoid sending incorrect to customer'
+workRealizationOneSecondP.textContent = 'Listen carefully to customers to better understand their needs and wants'
 workRealizationOneSecondDiv.append(spanWorkRealizationOneSecond, workRealizationOneSecondP)
 
 let workRealizationTwoSecondDiv = document.createElement("div")
@@ -465,28 +469,99 @@ workRealizationTwoSecondDiv.classList.add("flex-container")
 let spanWorkRealizationTwoSecond = document.createElement("span") 
 spanWorkRealizationTwoSecond.classList.add("circle")
 let workRealizationTwoSecondP = document.createElement("p")
-workRealizationTwoSecondP.textContent = 'Inspect working condition of my Order Picker to avoid possible issues or/and dowmturn'
+workRealizationTwoSecondP.textContent = 'Work with finance team to get customers approved for car loan'
 workRealizationTwoSecondDiv.append(spanWorkRealizationTwoSecond, workRealizationTwoSecondP)
 
 let workRealizationThreeSecondDiv = document.createElement("div")
-workRealizationThreeFirstDiv.classList.add("flex-container")
+workRealizationThreeSecondDiv.classList.add("flex-container")
 let spanWorkRealizationThreeSecond = document.createElement("span") 
 spanWorkRealizationThreeSecond.classList.add("circle")
 let workRealizationThreeSecondP = document.createElement("p")
-workRealizationThreeSecondP.textContent = 'Always read my scanner to avoid mistakes'
-workRealizationThreeFirstDiv.append(spanWorkRealizationThreeSecond, workRealizationThreeSecondP)
+workRealizationThreeSecondP.textContent = 'Contact customers by phone or email to achieve monthly sales goals'
+workRealizationThreeSecondDiv.append(spanWorkRealizationThreeSecond, workRealizationThreeSecondP)
 
 
 
-firstRowOneDiv.append(firstRowOneLeftDiv, firstRowOneRightDiv)
-firstRowTwoDiv.append(firstRowTwoLeftDiv, firstRowTwoRightDiv)
+secondRowOneDiv.append(secondRowOneLeftDiv, secondRowOneRightDiv)
+secondRowTwoDiv.append(secondRowTwoLeftDiv, secondRowTwoRightDiv)
 
 // second experience
 
-workDiv.append(workHeaderDiv, firstRowOneDiv, firstRowTwoDiv, workRealizationOneFirstDiv, workRealizationTwoFirstDiv, workRealizationThreeFirstDiv)
+workDiv.append(workHeaderDiv, firstRowOneDiv, firstRowTwoDiv, workRealizationOneFirstDiv, workRealizationTwoFirstDiv, workRealizationThreeFirstDiv, workNewLine, secondRowOneDiv, secondRowTwoDiv, workRealizationOneSecondDiv, workRealizationTwoSecondDiv, workRealizationThreeSecondDiv)
 
 
-// Work experience : Begin ===============================
+// Work experience : End ===============================
 
-resumeBodyDiv.append(educationDiv, skillsDiv, projectsDiv, workDiv)
+
+
+
+
+// Additional Work experience : Begin =====================
+let workAdditionalDiv = document.createElement("div") // main container
+let workHeaderAdditionalDiv = document.createElement("div")
+let workHeaderAddP = document.createElement("p")
+workHeaderAddP.classList.add("headerSection", "text-bold", 'top-margin')
+workHeaderAddP.textContent = "ADDITIONAL EXPERIENCE"
+workHeaderAdditionalDiv.append(workHeaderAddP)
+
+// let workNewLine = document.createElement("br")
+
+let additionalRowOneDiv = document.createElement("div")
+additionalRowOneDiv.classList.add("flex-container")
+let additionalRowLeftDiv = document.createElement("div")
+additionalRowLeftDiv.classList.add("left-row")
+let additionalRowLeftP =document.createElement("p")
+additionalRowLeftP.classList.add("text-bold")
+additionalRowLeftP.textContent = 'Les Centres Gheskio'
+additionalRowLeftDiv.append(additionalRowLeftP)
+
+let additionalRowRightDiv = document.createElement("div")
+additionalRowRightDiv.classList.add("right-row")
+let additionalRowRightP =document.createElement("p")
+additionalRowRightP.classList.add("text-bold")
+additionalRowRightP.textContent = 'Port-au-Prince, Haiti'
+additionalRowRightDiv.append(additionalRowRightP)
+
+let additionalRowTwoDiv = document.createElement("div")
+additionalRowTwoDiv.classList.add("flex-container")
+let additionalRowTwoLeftDiv = document.createElement("div")
+additionalRowTwoLeftDiv.classList.add("left-row")
+let additionalRowTwoLeftP =document.createElement("p")
+additionalRowTwoLeftP.textContent = 'Database manager'
+additionalRowTwoLeftDiv.append(additionalRowTwoLeftP)
+
+let additionalRowTwoRightDiv = document.createElement("div")
+additionalRowTwoRightDiv.classList.add("right-row")
+let additionalRowTwoRightP =document.createElement("p")
+// firstRowTwoRightP.classList.add("text-bold")
+additionalRowTwoRightP.textContent = 'Mar 2008 - May 2018'
+additionalRowTwoRightDiv.append(additionalRowTwoRightP)
+
+let additionalWorkRealizationOneDiv = document.createElement("div")
+additionalWorkRealizationOneDiv.classList.add("flex-container")
+let spanAdditionalWorkRealizationOne = document.createElement("span") 
+spanAdditionalWorkRealizationOne.classList.add("circle")
+let additionalWorkRealizationOneP = document.createElement("p")
+additionalWorkRealizationOneP.textContent = 'Create MS Access program to store patient\'s data to analyze monthly.'
+additionalWorkRealizationOneDiv.append(spanAdditionalWorkRealizationOne, additionalWorkRealizationOneP)
+
+let additionalWorkRealizationTwoDiv = document.createElement("div")
+additionalWorkRealizationTwoDiv.classList.add("flex-container")
+let spanAdditionalWorkRealizationTwo = document.createElement("span") 
+spanAdditionalWorkRealizationTwo.classList.add("circle")
+let additionalWorkRealizationTwoP = document.createElement("p")
+additionalWorkRealizationTwoP.textContent = 'Use SSRS to make patient\'s data dynamic and available in point in time.'
+additionalWorkRealizationTwoDiv.append(spanAdditionalWorkRealizationTwo, additionalWorkRealizationTwoP)
+
+
+
+additionalRowOneDiv.append(additionalRowLeftDiv, additionalRowRightDiv)
+additionalRowTwoDiv.append(additionalRowTwoLeftDiv, additionalRowTwoRightDiv)
+
+// second experience
+
+workAdditionalDiv.append(workHeaderAdditionalDiv, additionalRowOneDiv, additionalRowTwoDiv, additionalWorkRealizationOneDiv, additionalWorkRealizationTwoDiv)
+
+// Additional Work experience : End =====================
+resumeBodyDiv.append(educationDiv, skillsDiv, projectsDiv, workDiv, workAdditionalDiv)
 
